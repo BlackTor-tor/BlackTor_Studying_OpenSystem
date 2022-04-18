936dc94bd91c6c207f969c33ff25c57358359ab8
Cyanogen <2046015981@qq.com>
2022-04-18 12:33:22 +0800

    更新数据库表

--- src/main/resources/study.sql
+++ src/main/resources/study.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80011
  File Encoding         : 65001
 
- Date: 16/04/2022 23:51:28
+ Date: 18/04/2022 12:32:30
 */
 
 SET NAMES utf8mb4;
@@ -49,8 +49,8 @@ CREATE TABLE `clock_record`
     `createTime`    datetime(0)                                             NOT NULL COMMENT '创建时间',
     `clockTime`     datetime(0)                                             NOT NULL COMMENT '打卡时间',
     `endTime`       datetime(0)                                             NOT NULL COMMENT '结束时间',
-    `cover`         varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '打卡封面',
     `deleteTime`    datetime(0)                                             NULL     DEFAULT NULL COMMENT '删除时间',
+    `cover`         varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '打卡封面',
     `success`       tinyint(1)                                              NOT NULL DEFAULT 0 COMMENT '本次打卡是否成功',
     `isDelete`      tinyint(1)                                              NOT NULL DEFAULT 0 COMMENT '是否删除',
     PRIMARY KEY (`clockRecordId`) USING BTREE,
@@ -70,16 +70,37 @@ CREATE TABLE `clock_record`
 DROP TABLE IF EXISTS `invite_code`;
 CREATE TABLE `invite_code`
 (
-    `codeId`     int(11)                                                 NOT NULL AUTO_INCREMENT COMMENT '邀请码Id',
     `inviteCode` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '邀请码',
-    `bsosId`     varchar(256) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '创建者id',
-    PRIMARY KEY (`codeId`) USING BTREE,
-    INDEX `invite_code_user_bsosid_fk` (`bsosId`) USING BTREE,
-    CONSTRAINT `invite_code_user_bsosid_fk` FOREIGN KEY (`bsosId`) REFERENCES `user` (`bsosId`) ON DELETE RESTRICT ON UPDATE RESTRICT
+    `bsosId`     varchar(256) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '邀请人id',
+    `creatTime`  datetime(0)                                             NOT NULL COMMENT '创建时间',
+    `useCount`   bigint(20)                                              NOT NULL COMMENT '使用次数',
+    PRIMARY KEY (`inviteCode`) USING BTREE,
+    INDEX `invite_code_user_bsosId_fk` (`bsosId`) USING BTREE,
+    CONSTRAINT `invite_code_user_bsosId_fk` FOREIGN KEY (`bsosId`) REFERENCES `user` (`bsosId`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB
+  CHARACTER SET = utf8
+  COLLATE = utf8_general_ci COMMENT = '邀请码表'
+  ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for invite_code_record
+-- ----------------------------
+DROP TABLE IF EXISTS `invite_code_record`;
+CREATE TABLE `invite_code_record`
+(
+    `recordId`  bigint(20)                                              NOT NULL AUTO_INCREMENT COMMENT '记录id',
+    `bsosId`    varchar(256) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '邀请人id',
+    `invitedId` varchar(256) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '被邀请人id',
+    `usingTime` datetime(0)                                             NOT NULL COMMENT '使用时间',
+    PRIMARY KEY (`recordId`) USING BTREE,
+    INDEX `invite_code_record_user_bsosId_fk` (`bsosId`) USING BTREE,
+    INDEX `invite_code_record_user_bsosId_fk_2` (`invitedId`) USING BTREE,
+    CONSTRAINT `invite_code_record_user_bsosId_fk` FOREIGN KEY (`bsosId`) REFERENCES `user` (`bsosId`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+    CONSTRAINT `invite_code_record_user_bsosId_fk_2` FOREIGN KEY (`invitedId`) REFERENCES `user` (`bsosId`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE = InnoDB
   AUTO_INCREMENT = 1
   CHARACTER SET = utf8
-  COLLATE = utf8_general_ci COMMENT = '邀请码记录表'
+  COLLATE = utf8_general_ci
   ROW_FORMAT = Dynamic;
 
 -- ----------------------------
@@ -105,10 +126,10 @@ DROP TABLE IF EXISTS `note`;
 CREATE TABLE `note`
 (
     `noteId`      int(11)                                                 NOT NULL AUTO_INCREMENT COMMENT '笔记id',
-    `bsosId`      varchar(256) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '发布人id',
+    `bsosId`      varchar(256) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '发布人的用户id',
     `noteContent` text CHARACTER SET utf8 COLLATE utf8_general_ci         NOT NULL COMMENT '笔记内容',
     `files`       varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '上传文件',
-    `message`     text CHARACTER SET utf8 COLLATE utf8_general_ci         NOT NULL COMMENT '打卡提醒通知',
+    `message`     text CHARACTER SET utf8 COLLATE utf8_general_ci         NOT NULL COMMENT '打卡提醒通知内容',
     `remindTime`  datetime(0)                                             NOT NULL COMMENT '提醒时间',
     PRIMARY KEY (`noteId`) USING BTREE,
     INDEX `note_user_bsosid_fk` (`bsosId`) USING BTREE,
@@ -145,23 +166,23 @@ CREATE TABLE `real_message`
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`
 (
-    `bsosId`          varchar(256) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '平台标识/用户id',
-    `userId`          int(11)                                                 NOT NULL COMMENT '用户统计',
-    `nickName`        varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户昵称',
-    `userAccount`     varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '账号',
-    `password`        varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '密码',
-    `email`           varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '邮箱',
-    `recentLoginTime` datetime(0)                                             NOT NULL COMMENT '最近登录时间',
-    `registerTime`    datetime(0)                                             NOT NULL COMMENT '用户注册时间',
-    `avatar`          varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL     DEFAULT NULL COMMENT '头像',
-    `descriptions`    text CHARACTER SET utf8 COLLATE utf8_general_ci         NULL COMMENT '个人介绍',
-    `phone`           varchar(11) CHARACTER SET utf8 COLLATE utf8_general_ci  NULL     DEFAULT NULL COMMENT '手机号',
-    `age`             int(11)                                                 NULL     DEFAULT NULL COMMENT '年龄',
-    `problem`         varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL     DEFAULT NULL COMMENT '安全问题',
-    `ans`             varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL     DEFAULT NULL COMMENT '安全问题答案',
-    `isRealName`      tinyint(1)                                              NOT NULL DEFAULT 0 COMMENT '是否实名注册',
-    `score`           bigint(20)                                              NOT NULL DEFAULT 0 COMMENT '打卡得分',
-    `isEnable`        tinyint(1)                                              NOT NULL DEFAULT 1 COMMENT '用户是否可用',
+    `bsosId`             varchar(256) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户id',
+    `registrationNumber` int(11)                                                 NOT NULL COMMENT '注册序号',
+    `nickName`           varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户昵称',
+    `userAccount`        varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '账号',
+    `password`           varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '密码',
+    `email`              varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '邮箱',
+    `recentLoginTime`    datetime(0)                                             NOT NULL COMMENT '最近登录时间',
+    `registerTime`       datetime(0)                                             NOT NULL COMMENT '用户注册时间',
+    `avatar`             varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL     DEFAULT NULL COMMENT '头像',
+    `descriptions`       text CHARACTER SET utf8 COLLATE utf8_general_ci         NULL COMMENT '个人介绍',
+    `phone`              varchar(11) CHARACTER SET utf8 COLLATE utf8_general_ci  NULL     DEFAULT NULL COMMENT '手机号',
+    `age`                int(11)                                                 NULL     DEFAULT NULL COMMENT '年龄',
+    `problem`            varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL     DEFAULT NULL COMMENT '安全问题',
+    `ans`                varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL     DEFAULT NULL COMMENT '安全问题答案',
+    `isRealName`         tinyint(1)                                              NOT NULL DEFAULT 0 COMMENT '是否实名注册',
+    `score`              bigint(20)                                              NOT NULL DEFAULT 0 COMMENT '打卡得分',
+    `isEnable`           tinyint(1)                                              NOT NULL DEFAULT 1 COMMENT '用户是否可用',
     PRIMARY KEY (`bsosId`) USING BTREE
 ) ENGINE = InnoDB
   CHARACTER SET = utf8
